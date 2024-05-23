c471d4fe904c6acf1d0b8f0b8b5fe59fab22efb5
lauraxpb <l.pineda.2022@alumnos.urjc.es>
2024-05-23 13:52:27 +0200

    beginnings of infinitemode

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const inputControl = (readableString, exitOption, prev) => {
         //console.log(readableString);
         let choice = Number(customPrompt(readableString));
         var options = readableString.match(/\d+/g).map(Number);
-        
+
         if (choice === exitOption) {
             console.log("Exiting...");
             prev();
@@ -62,29 +62,47 @@ const MathQuestion = {
         return Math.floor(Math.random() * max);
     },
     fullQuestion: function (numSize) {
-        return `${this.generateOperand(numSize)}${this.generateOperator(
-            operators
-        )}${this.generateOperand(numSize)}`;
+        let operand1 = this.generateOperand(numSize);
+        let operand2 = this.generateOperand(numSize);
+        let operator = this.generateOperator(operators);
+
+        // Ensure the operation results in an integer
+        operand1 = operator === "/" ? operand1 * operand2 : operand1;
+
+        return `${operand1} ${operator} ${operand2}`;
     },
 };
 
-// TODO parchear divisiones entre 0 y numeros decimales 
+// TODO parchear divisiones entre 0 y numeros decimales
 
 const makeQuiz = (difficulty) => {
-    let questionObj = Object.create(MathQuestion);
-    let question = questionObj.fullQuestion(difficulty);
+    let answer;
+    let score = 0;
 
-    try {
-        while (eval(question) === parseInt(customPrompt(question))) {
-            question = questionObj.fullQuestion(difficulty);
+    while (true) {
+        let questionObj = Object.create(MathQuestion);
+        let question = questionObj.fullQuestion(difficulty);
+        answer = customPrompt(question);
+        if (isNaN(answer)) {
+            console.log("Enter a number");
+            continue;
         }
-    } catch {}
-    console.log("WRONG!");
+        if (eval(question) !== parseInt(answer)) {
+            break;
+        }
+        question = questionObj.fullQuestion(difficulty);
+        score++;
+    }
+    console.log(`WRONG! Your score was ${score}`);
     menu();
 };
 
 const startLevelQuiz = () => {
-    inputControl("Difficulty level:\n1: EASY\n2: MEDIUM\n3: HARD\n4:EXIT",4,menu)
+    inputControl(
+        "Difficulty level:\n1: EASY\n2: MEDIUM\n3: HARD\n4:EXIT",
+        4,
+        menu
+    )
         .then((level) => {
             makeQuiz(Math.pow(10, level));
         })
@@ -92,6 +110,14 @@ const startLevelQuiz = () => {
             console.log(error);
             startLevelQuiz();
         });
+};
+
+const startInfiniteQuiz = () => {
+    for (let i = 1; i < 4; i++) {
+        for (let j = 1; j < 6; j++) {
+            makeQuiz(Math.pow(10, i));
+        }
+    }
 };
 
 menu();
